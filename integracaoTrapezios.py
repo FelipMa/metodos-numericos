408b636d327c4c297beedaa7d2c20e77b928aa5e
Igor Nunes <inunes.us@gmail.com>
2023-07-10 21:07:26 -0300

    refactor: adicionando alguns comentarios e programas de testes

--- integracaoTrapezios.py
+++ integracaoTrapezios.py
@@ -4,8 +4,8 @@ import math
 def m_funcao(x, funcao=None) -> float:
     """
         Trasnforma a string na expressão matemática relacionada, devolvendo o valor da expressão.
-        A função aceita a constante matemática e = 2.718281..., log10(x) (log de x na base 10) e ln(x) (logaritmo
-        natural).
+        A função aceita a constante matemática e = 2.718281..., pi, log10(x) (log de x na base 10), ln(x) (logaritmo
+        natural), cos(s) e sen(x).
 
         Parâmetros:
             - x: valor da váriavel em que a expressão será calculada.
@@ -18,6 +18,12 @@ def m_funcao(x, funcao=None) -> float:
     if funcao is None:
         funcao = input()
 
+    # OBS: o método só é capaz de fazer a troca pelas funções da forma apresentada abaixo, o que significa que não
+    # funcionam através de uma composição delas. Por exemplo:
+    #  String aceita: 3*x**2 - 5x - 10
+    #  String não aceito: cos(x**2)
+    if funcao.__contains__("pi"):
+        funcao = funcao.replace("pi", str(math.pi))
     if funcao.__contains__("e"):
         funcao = funcao.replace("e", str(math.e))
     if funcao.__contains__("log(x)"):
@@ -39,10 +45,10 @@ def integracao_trapezios(funcao, a, b, n) -> float:
         Calcula a integral de uma função f(x) usando o método de integração dos Trapézios.
 
         Parâmetros:
-            - funcao: Função a ser integrada.
-            - a: Limite inferior do intervalo de integração.
-            - b: Limite superior do intervalo de integração.
-            - n: Número de subintervalos.
+            - funcao (string): Função a ser integrada.
+        - a (float): Limite inferior do intervalo de integração.
+        - b (float): Limite superior do intervalo de integração.
+        - n (int): Número de subintervalos.
 
         Retorna:
             - Aproximação da integral da função f(x) no intervalo [a, b].
@@ -68,10 +74,22 @@ def integracao_trapezios(funcao, a, b, n) -> float:
 
 
 if __name__ == "__main__":
-    funcao = input("Entre com f(x): ")
-    a = float(input("Limite inferior do intervalo: "))
-    b = float(input("Limite superior do intervalo: "))
-    n = int(input("Número de intervalos a serem usados: "))
+    # Programa para usar os métodos:
+    # funcao = input("Entre com f(x): ")
+    # a = float(input("Limite inferior do intervalo: "))
+    # b = float(input("Limite superior do intervalo: "))
+    # n = int(input("Número de intervalos a serem usados: "))
+    #
+    # soma = integracao_trapezios(funcao, a, b, n)
+    # print(f"Resultado da integral: {soma}")
 
-    soma = integracao_trapezios(funcao, a, b, n)
-    print(f"Resultado da integral: {soma}")
+    # Exemplo de utilização:
+    print("Exemplo de utilização: ")
+    f = "3*x**2 - 5x - 10 + pi"
+    a = 5
+    b = 10
+    n = 10000
+    print( f"função: {f};\n a: {a};\n b: {b};\n n: {n}")
+    resultado = integracao_trapezios(f, a, b, n)
+
+    print(f"'Valor da integral': {resultado}")
--- integracaoUmTercoSimpson.py
+++ integracaoUmTercoSimpson.py
@@ -4,8 +4,8 @@ import math
 def m_funcao(x, funcao=None) -> float:
     """
         Trasnforma a string na expressão matemática relacionada, devolvendo o valor da expressão.
-        A função aceita a constante matemática e = 2.718281..., log10(x) (log de x na base 10) e ln(x) (logaritmo
-        natural).
+        A função aceita a constante matemática e = 2.718281..., pi, log10(x) (log de x na base 10), ln(x) (logaritmo
+        natural), cos(s) e sen(x).
 
         Parâmetros:
             - x: valor da váriavel em que a expressão será calculada.
@@ -18,6 +18,12 @@ def m_funcao(x, funcao=None) -> float:
     if funcao is None:
         funcao = input()
 
+    # OBS: o método só é capaz de fazer a troca pelas funções da forma apresentada abaixo, o que significa que não
+    # funcionam através de uma composição delas. Por exemplo:
+    #  String aceita: 3*x**2 - 5x - 10
+    #  String não aceito: cos(x**2)
+    if funcao.__contains__("pi"):
+        funcao = funcao.replace("pi", str(math.pi))
     if funcao.__contains__("e"):
         funcao = funcao.replace("e", str(math.e))
     if funcao.__contains__("log(x)"):
@@ -36,13 +42,14 @@ def m_funcao(x, funcao=None) -> float:
 
 def integracao_1_3_simpson(funcao, a, b, n):
     """
-        Calcula a integral de uma função f(x) usando o método de integração 1/3 de Simpson.
+        Calcula a integral de uma função f(x) usando o método de integração 1/3 de Simpson. Para o funcionamento da
+        função, o número de subintervalos deve ser par.
 
         Parâmetros:
-        - funcao: Função a ser integrada.
-        - a: Limite inferior do intervalo de integração.
-        - b: Limite superior do intervalo de integração.
-        - n: Número de subintervalos (deve ser par).
+        - funcao (string): Função a ser integrada.
+        - a (float): Limite inferior do intervalo de integração.
+        - b (float): Limite superior do intervalo de integração.
+        - n (int): Número de subintervalos (deve ser par).
 
         Retorna:
         - Aproximação da integral da função f(x) no intervalo [a, b].
@@ -79,10 +86,22 @@ def integracao_1_3_simpson(funcao, a, b, n):
 
 
 if __name__ == "__main__":
-    funcao = input("Entre com f(x): ")
-    a = float(input("Limite inferior do intervalo: "))
-    b = float(input("Limite superior do intervalo: "))
-    n = int(input("Número de intervalos a serem usados: "))
+    # Programa para usar os métodos:
+    # funcao = input("Entre com f(x): ")
+    # a = float(input("Limite inferior do intervalo: "))
+    # b = float(input("Limite superior do intervalo: "))
+    # n = int(input("Número de intervalos a serem usados: "))
+    #
+    # soma = integracao_trapezios(funcao, a, b, n)
+    # print(f"Resultado da integral: {soma}")
 
-    soma = integracao_1_3_simpson(funcao, a, b, n)
-    print(f"Resultado da integral: {soma}")
+    # Exemplo de utilização:
+    print("Exemplo de utilização: ")
+    f = "3*x**2 - 5x - 10 + cos(x**2)"
+    a = 5
+    b = 10
+    n = 10000
+    print(f" função: {f};\n a: {a};\n b: {b};\n n: {n}")
+    resultado = integracao_1_3_simpson(f, a, b, n)
+
+    print(f"'Valor da integral': {resultado}")
